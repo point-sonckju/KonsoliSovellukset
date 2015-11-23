893c8ea25a44edc7b2bb3a1a7a944929ed498157
Juha Sonck <juha.sonck@pointcollege.fi>
2015-11-23 15:18:32 +0200

    Lisätty tiedoston lukuruutiinia

--- WPFTiedonHallintaSovellus/WPFTiedonHallintaSovellus/Program.cs
+++ WPFTiedonHallintaSovellus/WPFTiedonHallintaSovellus/Program.cs
@@ -11,17 +11,23 @@ namespace WPFTiedonHallintaSovellus
     {
         static void Main(string[] args)
         {
+            //Tiedostoon tallentaminen
+            string tiedosto = @"C:\Temp\Lämpötila.txt"; //miukumerkki tarkoittaa että kenomerkki ei olekaan ESC -merkki!!!
+
             //Tähän toteutetaan tiedoston luku
-            
-            
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine("Aiemmin asetettu lämpötila on: " + aiempiArvo + " astetta.");
+            }
+
             //Kysytään käyttäjältä uutta lämpötilaa ja tallentaan se tiedostoon
             int lämpötila = 0;
             Console.Write("Anna uusi lämpötila: ");
             string syöte = Console.ReadLine();
             lämpötila = int.Parse(syöte);
 
-            //Tiedostoon tallentaminen
-            string tiedosto = @"C:\Temp\Lämpötila.txt"; //miukumerkki tarkoittaa että kenomerkki ei olekaan ESC -merkki!!!
+
 
             File.WriteAllText(tiedosto, lämpötila.ToString());
             Console.WriteLine("Uusi lämpötila " + lämpötila.ToString() + " on tallennettu");
